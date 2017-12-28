2ef0824f09040baacb74717d9e25f6ceaf397ddc
Sanghyuk <saanghyuk@gmail.com>
2017-12-28 22:20:13 +0900

    heroku3

--- server.js
+++ server.js
@@ -48,8 +48,7 @@ io.on('connection', (socket)=>{
 
 
 
-
-server.listen(process.env.port || 3000, ()=>{
-    console.log('server is on')
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
